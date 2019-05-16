615a102dcdb0a6b4974bb7717381de2d526e486b
Roamin <r.soubrane@icloud.com>
2019-05-16 15:56:17 +0200

    none

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-let quizData = require('./quiz_data.json')
+let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    state = {
-        quiz_position: 1
+    constructor(props) {
+        super(props)
+        this.state = {
+            quiz_position: 1
+        }
     }
 
     render() {
         return (
             <div>
                 <div className="QuizQuestion">
-                    {quizData.quiz_question[0].instruction_test}
+                    {quizData.quiz_question[0].instruction_text}
                 </div>
             </div>
         )
     }
-}
+};
 
-export default Quiz
+export default Quiz;
