ba5124703dd20fb6e26ffbf62d7729c32ae316c4
Roamin <r.soubrane@icloud.com>
2019-05-16 15:53:37 +0200

    Module 1 ocmpleted

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,11 +3,8 @@ import React, { Component } from 'react'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            quiz_position: 1
-        }
+    state = {
+        quiz_position: 1
     }
 
     render() {
